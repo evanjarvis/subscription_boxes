6f5bccaa7336fc3806db32a0439b45492229430c
Evan Jarvis <ejarvis@umw.edu>
2016-06-08 14:39:16 -0400

    Added templates folder and load_content.js

---
+++ load_content.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	
+	var newhash = "",
+		$content = $("#content"),
+		$wrapper = $("#wrapper"),
+		baseHeight = 0,
+		$el;
+		
+		$wrapper.height($wrapper.height());
+		$baseHeight = $("#wrapper").height()
+		
+	$("body").delegate("a", "click", function() {
+    	window.location.hash = $(this).attr("href");
+        return false;
+    });
+	
+	$(window).bind('hashchange', function(){
+		
+		newHash = window.location.hash.substring(1);
+		
+		if (newHash) {
+			
+			$wrapper
+				.find("#guts")
+				.fadeOut(200, function(){ 
+				$content.hide().load("templates/" + newHash + "#guts", function(){
+					$content.fadeIn(200, function(){
+						$content.animate({
+							//height: 932 +"px"
+						});
+					});
+					$(".wrapper a").removeClass("current");
+                    $(".wrapper a[href="+newHash+"]").addClass("current");
+				});newHash});newHash};
+	});
+	$(window).trigger('hashchange');
+});
+	
---
+++ templates/signup.php
@@ -0,0 +1,7 @@
+
+<div id "guts">
+	<form id="login">
+	<input type="text" value="test field">
+	<input type="submit">
+	</form>
+</div>
