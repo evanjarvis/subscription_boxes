9fc126a15332b6043924597856df620f6ca7cb4f
Evan Jarvis <ejarvis@umw.edu>
2016-06-09 15:27:12 -0400

    added new account form

---
+++ templates/new_account.php
@@ -0,0 +1,19 @@
+<div id="guts">
+
+<form id="register">
+	<h3>Create an account:</h3>
+	First name:<br>
+	<input type="text" id="first_name" required/><br>
+	Last name:<br>
+	<input type="text" id="last_name" required/><br>
+	Email:<br>
+	<input type="text" id="email" required><br>
+	Create password:<br>
+	<input type="password" id="password" required/>
+	Confirm password:
+	<input type="password" id="password_confirm"required/><br><br>
+
+	<input type="submit" value="create account"required/>
+</form
+
+</div
