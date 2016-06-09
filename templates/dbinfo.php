31508cfb39707bda931847b440b5948fcb2c5b3f
Evan Jarvis <ejarvis@umw.edu>
2016-06-09 11:40:12 -0400

    Added login page logic

---
+++ templates/dbinfo.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost = 'localhost';
+$dbuser = 'chall7';
+$dbpass = 'password';
+$dbname = 'cpsc348_chall7';
+?>
--- templates/signup.php
+++ templates/signup.php
@@ -1,10 +1,25 @@
 <div id="guts">
 
-	<form id="login" method="POST">
-	<input type="text" placeholder="Username"><br>
-	<input type="password" placeholder="Password" style="margin-top:5px"><br>
-	<input type="submit" value="Login" style="margin-top:5px">
-	</form>
-	<p>New user? <a href="new_account.php">Create an Account<a></p>
-
+<?php
+session_start();
+if (isset($_SESSION["username"]) && !empty(!$session["username"])){
+	echo('USER DASHBOARD');
+} else {
+	echo('
+		<h3>DISPLAY IF NO SESSION:</h3>
+		<form id="login" method="POST">
+		<input type="text" placeholder="Username"><br>
+		<input type="password" placeholder="Password" style="margin-top:5px"><br>
+		<input type="submit" value="Login" style="margin-top:5px">
+		</form>
+		<p>New user? <a href="new_account.php">Create an Account</a></p>
+		');
+}
+/*
+function logout(){
+	if(isset($_SESSION['username'])){
+		unset($_SESSION['username']);
+	}
+*/
+?>
 </div>
