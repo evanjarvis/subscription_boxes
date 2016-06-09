3811d7fa460eb83f36dcfd04dfc2afd227dac05a
Michael Bloecker <mbloecke@rosemary@umw.edu>
2016-06-09 14:25:37 -0400

    updated sidenav with session

---
+++ sidenav.php
@@ -0,0 +1,19 @@
+	<?php
+	session_start(); 
+	$_SESSION["username"] = 'Altarian';
+	session_unset();
+ ?>
+	
+	<ul id="sidenav">
+	<li><a href="home.php">Home</a></li>
+	<li><a href="signup.php">Sign Up</a></li>
+	<li><a href="contact.php">Contact Us</a></li>
+        <?php if (isset($_SESSION["username"]) && !empty(!$session["username"])){
+        echo('<li><a href="logout.php" onclick="logout()">Logout</a></li>');
+        } else {
+        echo('<li><a href="signup.php">Login</a></li>');
+	}
+        ?>
+
+	</ul>
+
