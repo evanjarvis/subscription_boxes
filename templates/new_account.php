da7832bd416a0ef533f11be7ba90de639607b46f
Michael Bloecker <mbloecke@rosemary@umw.edu>
2016-06-11 18:58:34 -0400

    edited form for signup

--- templates/new_account.php
+++ templates/new_account.php
@@ -1,19 +1,21 @@
 <div id="guts">
 
-<form id="register">
+<form id="register" action="templates/register.php" method="POST">
 	<h3>Create an account:</h3>
 	First name:<br>
-	<input type="text" id="first_name" required/><br>
+	<input type="text" id="first_name" name="first_name" required/><br>
 	Last name:<br>
-	<input type="text" id="last_name" required/><br>
+	<input type="text" id="last_name" name="last_name" required/><br>
 	Email:<br>
-	<input type="text" id="email" required><br>
+	<input type="text" id="email" name="email" required><br>
+	Shipping address for boxes:<br>
+	<input type="text" id="address" name="address" required><br>
 	Create password:<br>
-	<input type="password" id="password" required/>
+	<input type="password" id="password" name="password" required/>
 	Confirm password:
-	<input type="password" id="password_confirm"required/><br><br>
+	<input type="password" id="password_confirm" name="password_confirm" required/><br><br>
 
-	<input type="submit" value="create account"required/>
-</form
+	<input type="submit" name="submit" value="create account"required/>
+</form>
 
-</div
+</div>
--- templates/register.php
+++ templates/register.php
@@ -1,40 +1,61 @@
-<!-- register.php -->
-<html>
-	<head>
-		<title></title>
-	</head>
-	<body>
-	<form> 
+<!doctype html>
+<html lang="eng-US">
+<head>
+<meta charset="utf-8">
+<title>UMW Subscription Boxes</title>
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+<!--link to jquery librarys -->
+<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js'></script>
+<script type='text/javascript' src='js/jquery.ba-hashchange.min.js'></script>
+<script src="../load_content.js"></script>
+</head>
+
+<body>
+
+<div id="wrapper">
+        <div id="masthead">
+<!--            <h1>EagleBox</h1>
+        <h3>(Study-Abroad subscripton box service)</h3> -->
+        </div> <!-- end masthead -->
+        <div id="pageblock">
+                <div class="block" style="width: 75%; float:left">
+                <div id="content">
+			<div id="guts">
 	<?php
 		//establish database connection
 		include('dbinfo.php');
 		$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
 		if(!$conn){
-			echo ("<p> Unable to process your request.\n" . 
-					"Please try again later.</p>");
+			echo ("<p> Unable to process your request.\nPlease try again later.</p>");
 			exit();
 		}
 
 		//begin registration
 		if(isset($_POST['submit'])){
 			$msg = NULL;
-
-			//checks username
-			if(empty($_POST['username'])){
-				$username = FALSE;
-				$msg .= '<p>You must enter in a username.</p>';
+			//checks first name
+			if(empty($_POST["first_name"])){
+				$fname = FALSE;
+				$msg .= '<p>You must enter in a first name.</p>';
 			}else{
-				$username = mysqli_real_escape_string($conn, $_POST['username']);
+				$fname = mysqli_real_escape_string($conn, $_POST['first_name']);
 			}
 
+                        //checks last name
+                        if(empty($_POST["last_name"])){
+                                $lname = FALSE;
+                                $msg .= '<p>You must enter in a last name.</p>';
+                        }else{
+                                $lname = mysqli_real_escape_string($conn, $_POST['last_name']);
+                        }
+
 			//checks password
-			if(empty($_POST['password1'])){
+			if(empty($_POST['password'])){
 				$password = FALSE;
 				$msg .= '<p>You must enter in a password.</p>';
 			}else{
-				if($_POST['password1'] == $_POST['password2']){
-					$password = mysqli_real_escape_string($conn, $_POST['password1']);
+				if($_POST['password'] == $_POST['password_confirm']){
+					$password = mysqli_real_escape_string($conn, $_POST['password']);
 				}else{
 					$password = FALSE;
 					$msg .= '<p>The passwords you entered do not match.</p>';
@@ -58,32 +79,54 @@
 			}
 
 			//when username, password, and email are good
-			if($username && $password && $address && $email){
+			if($fname && $lname && $password && $address && $email){
 				//where SELECT $query info goes for subscription database
+				$query = "SELECT * FROM sub_users WHERE email='$email'";
 				$result = @mysqli_query($conn, $query);
 				if(mysqli_num_rows($result) == 0){
 					//where INSERT $query info gets inserted into subscription database
+					$query = "INSERT INTO sub_users (first_name, last_name, email, address, user_password) VALUES('$fname', '$lname', '$email', '$address', '$password')";
 					$result = @mysqli_query($conn, $query);
-				}
 				//when registration info is good
-				if($result){
-					echo '<p>You have successfully registered.</p>';
-				}else{
-					$msg .= '<p>We could not register you at this time.</p><p>' . 
+					if($result){
+						echo '<p>You have successfully registered.</p>';
+					}else{
+						$msg .= '<p>We could not register you at this time.</p><p>' . 
 						mysqli_error() . '</p>';
+					}
+
+				}else{
+					$msg .= '<p>That email is already in use.</p>';
 				}
+				mysqli_close();
 			}else{
-				$msg .= '<p>That username is already taken.</p>';
+				$msg .= '<p>Error registering informatiion. Please try again later.</p>';
 			}
-			mysqli_close();
-		}else{
-			$msg .= '<p>Please try again later.</p>';
 		}
-		
 		//prints any error messages
 		if(isset($msg)){
 			echo $msg;
 		}
 	?>
-	</body>
+			</div>
+                </div> <!--end content-->
+                </div> <!-- end block-->
+                <div class="block" style="width:25%; float:right">
+                <nav id="nav01"></nav>
+                </div>
+        </div><!-- end pageblock -->
+    <footer style="float:left">
+    <p style="color:white">&copy; <script language="JavaScript">
+        <!--
+    today=new Date();
+    year0=today.getFullYear();
+    document.write(year0);
+        //-->
+        </script>.  All Rights Reserved.</p>
+</footer>
+</div> <!-- end wrapper -->
+<div id="galleryContainer"></div>
+<script src="../javascript/functions.js"></script>
+<script src="../javascript/navbar.js"></script>
+</body>
 </html>
