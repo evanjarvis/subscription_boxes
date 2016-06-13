7092b32a03764ec556bce2436de87649e4031a7b
Michael Bloecker <mbloecke@rosemary@umw.edu>
2016-06-13 10:49:36 -0400

    side nav no login

--- sidenav.php
+++ sidenav.php
@@ -8,12 +8,5 @@
 	<li><a href="home.php">Home</a></li>
 	<li><a href="signup.php">Sign Up</a></li>
 	<li><a href="contact.php">Contact Us</a></li>
-        <?php if (isset($_SESSION["username"]) && !empty(!$session["username"])){
-        echo('<li><a href="signup.php" onclick="logout()">Logout</a></li>');
-        } else {
-        echo('<li><a href="signup.php">Login</a></li>');
-	}
-        ?>
-
 	</ul>
 
